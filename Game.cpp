b7a99dc73490182f90fd5ef236c425066e4c7116
Legion <ryosukekageyama13@gmail.com>
2026-02-05 18:31:27 +0900

    no message

--- Game.cpp
+++ Game.cpp
@@ -13,7 +13,6 @@ void Game::Initialize()
 	player->transform().position() = Vector3(0.0f, 1.0f, 0.0f);
 
 	StaticCubePrefab cubePrefab{};
-	cubePrefab.TexturePtr = resource().Load<Texture>(L"texture/ground.png");
 	GameObject* ground = Instantiate(cubePrefab);
 	ground->transform().scale() = Vector3(100.0f, 10.0f, 100.0f);
 }
