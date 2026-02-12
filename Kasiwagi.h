05a7ce8ab3b4e37d8b4b13a971fd93002bbf845f
Legion <ryosukekageyama13@gmail.com>
2026-02-12 13:40:35 +0900

    no message

---
+++ Kasiwagi.h
@@ -0,0 +1,10 @@
+#ifndef KASIWAGI_H
+#define KASIWAGI_H
+
+class Kasiwagi
+{
+public:
+	void Initialize();
+};
+
+#endif
---
+++ Namioka.h
@@ -0,0 +1,11 @@
+#ifndef NAMIOKA_H
+#define NAMIOKA_H
+
+#include "Scene.h"
+
+class Namioka : public Scene
+{
+public:
+	void Initialize() override;
+};
+#endif
